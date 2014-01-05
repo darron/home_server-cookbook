14da7ac00544202df73e7ce413b024c94ea633a7
Darron Froese <darron@froese.org>
2014-01-04 19:58:44 -0700

    First commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+name             'home-server'
+maintainer       'Darron Froese'
+maintainer_email 'darron@froese.org'
+license          'Apache 2.0'
+description      'Installs/configures something'
+version          '0.1.0'
+recipe           'home-server::default', 'Installs/configures something'
